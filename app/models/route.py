c0674dbc8d545ff8d76afffc0d8296bd77355d64
Mureithi-Code <darkknight505c.com>
2025-02-21 13:01:24 +0300

    database setup

--- app/models/route.py
+++ app/models/route.py
@@ -6,3 +6,4 @@ class Route(db.Model):
     destination = db.Column(db.String(100), nullable=False)
     departure_time = db.Column(db.DateTime, nullable=False)
     bus_id = db.Column(db.Integer, db.ForeignKey('bus.id'))
+    cost = db.Column(db.Integer, nullable=False)
